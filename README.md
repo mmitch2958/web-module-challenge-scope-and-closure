3af21f47ab3c2c9fb90f664e978c26a407dc1cc3
mmitch2958 <mikescottmitchell@outlook.com>
2020-07-20 20:05:22 -0400

    last min code clean up

--- README.md
+++ README.md
@@ -41,7 +41,7 @@ Find the file `index.js` and complete the tasks.
 
 Edit the `ReadMe` file with your answers.
 
-1. In your own words, define closure (1-2 sentences).
+1. In your own words, define closure (1-2 sentences). 
 2. Study the following code, then answer the questions below.
 
 ```js
--- index.js
+++ index.js
@@ -45,21 +45,21 @@
 */
 
 // counter1 code
-// function counterMaker() {
-//   let count = 0;
-//   return function counter() {
-//    return count++;
-//   }
-// }
+function counterMaker() {
+  let count = 0;
+  return function counter() {
+    return count++;
+  }
+}
 
-// const counter1 = counterMaker();
+const counter1 = counterMaker();
 
-// // counter2 code
-// let count = 0;
+// counter2 code
+let count = 0;
 
-// function counter2() {
-//   return count++;
-// }
+function counter2() {
+  return count++;
+}
 
 
 /* Task 2: inning() 
@@ -69,113 +69,16 @@ between 0 and 2. */
 
 
 
-// function inning(){
-//   return Math.floor(Math.random() * 3);
-//  }
-//  console.log(inning());
-//  console.log(inning());
-//  console.log(inning());
-//  console.log(inning());
-
-/* Task 3: finalScore()
-
-Write a higher order function called `finalScore` that accepts the callback function `inning` 
-(from above) and a number of innings and and returns the final score of the game in the form of an object */
-
-
-
-//  finalScore(inning, 9) might return: 
-//  {
-//    "Home": 11,
-//    "Away": 5,
-//  }
-
-// let scoreArr[]
- 
-//  function finalScore(varInning, numInnings){
-//    homeScore = 0;
-//    awayScore = 0;
-//    for (i = 0 ; i < numInnings ; i++) {
-//      homeScore = homeScore + inning();
-//    }
-//    for (i = 0 ; i < numInnings ; i++) {
-//      awayScore = awayScore + inning();
-//    }
-//    return `"Home", ${homeScore}, "Away", ${awayScore}`;
-//  }
-//  console.log(finalScore(inning(),1));
-
-// const finalScore = function(func,num){
-//   let score = {
-//     "Home": 0,
-//     "Away": 0
-//   }
-//   for (let i = 0; i < num; i++){
-//     score.Home += func()
-//     score.Away += func()
-//   }
-//   return score
-// }
-// console.log(finalScore(inning,9))
-
-
-// function scoreboard(funcInning,funcScore,num) {
-//   let total = {
-//     'Home"'
-//   }
-//   for(let i = 1; i <= num;i++){
-//     let score = funcScore(funcInning,1)
-//     total.Home += score.Home;
-//     total.Away += score.Away;
-//     console.log(`Inning ${i}: Home Score = ${score.Home} Away Score is ${score.Away}`)
-//   }
-//   return `Final Score: Home = ${total.Home} Away = ${total.Away}`
-// }
-// console.log(scoreboard(inning,finalScore,9))
-
-// /* Task 4: 
-
-// Create a function called `scoreboard` that accepts the following parameters: 
-
-// (1) Callback function `getInningScore`
-// (2) Callback function `inning`
-// (3) A number of innings
-
-// and returns the score at each pont in the game, like so:
-// 1st inning: awayTeam - homeTeam
-// 2nd inning: awayTeam - homeTeam
-// 3rd inning: awayTeam - homeTeam
-// 4th inning: awayTeam - homeTeam
-// 5th inning: awayTeam - homeTeam
-// 6th inning: awayTeam - homeTeam
-// 7th inning: awayTeam - homeTeam
-// 8th inning: awayTeam - homeTeam
-// 9th inning: awayTeam - homeTeam
-// Final Score: awayTeam - homeTeam */
-
-
-
-// function scoreboard(funcInning,funcScore,num) {
-//   let total = {
-//     'Home"'
-//   }
-//   for(let i = 1; i <= num;i++){
-//     let score = funcScore(funcInning,1)
-//     total.Home += score.Home;
-//     total.Away += score.Away;
-//     console.log(`Inning ${i}: Home Score = ${score.Home} Away Score is ${score.Away}`)
-//   }
-//   return `Final Score: Home = ${total.Home} Away = ${total.Away}`
-// }
-// console.log(scoreboard(inning,finalScore,9))
-
-
-
-
-
-const inning = function(){
-    return Math.floor(Math.random() * 3)
+function inning() {
+  return Math.floor(Math.random() * 3);
 }
+console.log(inning());
+
+
+
+// const inning = function(){
+//     return Math.floor(Math.random() * 3) 
+// }
 /* Task 3: finalScore()
 Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
 For example, 
@@ -184,19 +87,21 @@ finalScore(inning, 9) might return:
   "Home": 11,
   "Away": 5,
 }
-*/ 
-const finalScore = function(func,num){
+*/
+const finalScore = function (func, num) {
   let score = {
     "Home": 0,
-    "Away":0
+    "Away": 0
   }
-  for (let i = 0; i < num; i++){
+  for (let i = 0; i < num; i++) {
     score.Home += func()
     score.Away += func()
   }
   return score
 }
-console.log(finalScore(inning,9))
+console.log(finalScore(inning, 9))
+
+
 /* Task 4: 
 Create a function called `scoreboard` that accepts the following parameters: 
 (1) Callback function `getInningScore`
@@ -214,17 +119,17 @@ and returns the score at each pont in the game, like so:
 9th inning: awayTeam - homeTeam
 Final Score: awayTeam - homeTeam */
 
-function scoreboard(funcInning,funcScore,num) {
+function scoreboard(funcInning, funcScore, num) {
   let total = {
-    "Home":0,
-    "Away":0
+    "Home": 0,
+    "Away": 0
   }
-  for(let i = 1; i <= num;i++){
-    let score = funcScore(funcInning,1)
+  for (let i = 1; i <= num; i++) {
+    let score = funcScore(funcInning, 1)
     total.Home += score.Home;
     total.Away += score.Away;
-    console.log(`Inning ${i}: Home Score = ${score.Home} Away Score is ${score.Away}`)
+    console.log(`Inning ${i}: Home Score: ${total.Home} Away Score: ${total.Away}`)
   }
   return `Final Score: Home = ${total.Home} Away = ${total.Away}`
 }
-console.log(scoreboard(inning,finalScore,9))
+console.log(scoreboard(inning, finalScore, 9))
